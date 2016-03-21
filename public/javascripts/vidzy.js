dc83649bed79ced0446e3dc803ee1d90eb739356
agustinbalquin <agustinbalquin@gmail.com>
2016-03-21 02:32:36 -0700

    Added ngRoute dependency and replaced jade

--- public/javascripts/vidzy.js
+++ public/javascripts/vidzy.js
@@ -1,1 +1,13 @@
-var app = angular.module('Vidzy', []);
+//var app = angular.module('Vidzy', []);
+//added dependencies of ngRoute (Angular module)
+var app = angular.module('Vidzy', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+        .when('/', {
+            templateUrl: 'partials/home.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+}]);
